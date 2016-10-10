cb6040d3df59a329416b77778cec5f6ef480dcef
mateuswetah <mateus.m.luna@gmail.com>
2016-10-10 12:57:26 -0300

    First Commit

---
+++ app/auth0-variables.ts
@@ -0,0 +1,5 @@
+export class Auth0Vars {
+    static AUTH0_CLIENT_ID = "hEecTNdtmGxPZEmxeAacSogleAkKon0X";
+    static AUTH0_CALLBACK_URL = location.href;
+    static AUTH0_DOMAIN = "mateus-sas.auth0.com";
+}
